31c6594e3ff9aae426d64f1c289ea091c09bf33c
Rider <locochon56303@gmail.com>
2024-10-01 22:00:50 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+// Calculator: -- - - - 
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+function clearDisplay(input){
+    display.value ="";
+    
+}
+function calculate(input){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "Error"
+    }
+}
